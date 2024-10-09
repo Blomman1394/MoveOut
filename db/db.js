8ad272e1d0c7621f1e26c0dc949d2d8e42272c59
Alexander Blomqvist <153428819+Blomman1394@users.noreply.github.com>
2024-10-09 18:15:49 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,10 +2,12 @@ const mysql = require('mysql');
 
 // MySQL connection configuration
 const dbOptions = {
-    host: "MSI.local",
-    user: "dbadm",
-    password: "P@ssw0rd",
-    database: "iLabel"
+    host: process.env.DB_HOST,       // use the environment variable
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: 3306,                       // default MySQL port
+    multipleStatements: true
 };
 
 // Create a connection pool
