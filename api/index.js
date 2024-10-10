efe1c181a0e530e88a4b1f2041808554b38b2272
Blomman1394 <pt.alexanderblomqvist@gmail.com>
2024-10-10 10:03:04 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Set the view engine and views directory
 app.set("view engine", "ejs");
-app.set('views', './views');
+app.set('views', path.join(__dirname, '../views'));
 
 // Middleware to log incoming requests
 app.use(middleware.logIncomingToConsole);
