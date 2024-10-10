493878e13d9c90c998890a21a2f0765e6ca6b99f
Blomman1394 <pt.alexanderblomqvist@gmail.com>
2024-10-10 09:11:53 +0200

    New index.js
    new index + vercel

--- api/index.js
+++ api/index.js
@@ -1,25 +1,13 @@
-/**
- * A sample Express server with static resources.
- */
 "use strict";
-const cors = require('cors');
-const port = 3306;
+
 const path = require("path");
 const express = require("express");
-const mysql = require('mysql');
-const app = express();
-const db = require('./db/db');
-const routeshop = require("./route/exam.js");
-const middleware = require("./middleware/index.js");
 const session = require('express-session');
+const middleware = require("../middleware/index.js");
+const routeshop = require("../route/exam.js");
 
+const app = express();
 
-const corsOptions = {
-    origin: 'https://cabhholding.se', // Allow requests from your domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-};
-app.use(cors(corsOptions));
 // Set the view engine and views directory
 app.set("view engine", "ejs");
 app.set('views', './views');
@@ -28,13 +16,14 @@ app.set('views', './views');
 app.use(middleware.logIncomingToConsole);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/uploads', express.static('uploads'));
+
 // Session middleware should come before routes
 app.use(session({
     secret: 'Oswald', // Change this to a strong secret
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24, 
+        maxAge: 1000 * 60 * 60 * 24,
         secure: false, // Set to true if using HTTPS
     }
 }));
@@ -54,33 +43,5 @@ app.use(express.static(path.join(__dirname, "public")));
 // Route definitions should come after session middleware
 app.use("/", routeshop);
 
-// Start the server and log startup details
-app.listen(port, logStartUpDetailsToConsole);
-
-/**
- * Log app details to console when starting up.
- *
- * @return {void}
- */
-function logStartUpDetailsToConsole() {
-    let routes = [];
-
-    // Find what routes are supported
-    app._router.stack.forEach((middleware) => {
-        if (middleware.route) {
-            // Routes registered directly on the app
-            routes.push(middleware.route);
-        } else if (middleware.name === "router") {
-            // Routes added as router middleware
-            middleware.handle.stack.forEach((handler) => {
-                let route;
-                route = handler.route;
-                route && routes.push(route);
-            });
-        }
-    });
-
-    console.info(`Server is listening on port ${port}.`);
-    console.info("Available routes are:");
-    console.info(routes);
-}
+// Export the app as a module for Vercel to use
+module.exports = app;
